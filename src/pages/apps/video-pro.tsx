2c5a72d082e198022ee434356027e51ccf5ffe32
tomexgsm-dev <tomexgsm@gmail.com>
2026-03-26 17:02:12 +0100

    Add VideoProPage component for video presentation

---
+++ src/pages/apps/video-pro.tsx
@@ -0,0 +1,9 @@
+import VideoPro from "@/components/VideoPro";
+
+export default function VideoProPage() {
+  return (
+    <div className="min-h-screen bg-[#050509] text-white">
+      <VideoPro />
+    </div>
+  );
+}
