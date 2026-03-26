280f837fafc2902c04f9f8f384f303a2b22abaf7
tomexgsm-dev <tomexgsm@gmail.com>
2026-03-26 19:43:19 +0100

    Add VideoProPage component with layout

--- supabase/functions/video-pro/index.ts
+++ supabase/functions/video-pro/index.ts
@@ -1,1 +1,11 @@
+import VideoPro from "@/pages/VideoPro";
+import VideoProLayout from "@/components/VideoProLayout";
+
+export default function VideoProPage() {
+  return (
+    <VideoProLayout>
+      <VideoPro />
+    </VideoProLayout>
+  );
+}
 
